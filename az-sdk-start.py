9b8af3d2bdc6f8d04d105379c5088181ca29ca61
Johnny Pantelimon <johnny.pantelimon@gmail.com>
2024-11-13 19:55:55 +0000

    new

--- az-sdk-start.py
+++ az-sdk-start.py
@@ -1,6 +1,7 @@
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
+from azure.storage.blob import BlobServiceClient
 import os
 import config
 
@@ -9,6 +10,7 @@ azure_subscription_id = os.environ["az_subscription"]
 rg_name = "azpythonsdk"
 az_loc = "uksouth"
 storage_name = "azpythonsdksa0806"
+container_name = "container-1"
 
 try:
     creds = AzureCliCredential()
@@ -29,23 +31,54 @@ except Exception as ex:
 
 try:
     storage_client = StorageManagementClient(credential=creds, subscription_id=azure_subscription_id)
+    
+    if len([item.name for item in storage_client.storage_accounts.list() if item.name == storage_name]) == 0:
 
-    check_name_result = storage_client.storage_accounts.check_name_availability({"name" : storage_name})
+        check_name_result = storage_client.storage_accounts.check_name_availability({"name" : storage_name})
 
-    if not check_name_result.name_available:
-        print(f"Storage account {storage_name} already exists")
+        if not check_name_result.name_available:
+            print(f"Global Storage account name {storage_name} already exists")
+        else:
+            poller = storage_client.storage_accounts.begin_create(
+                resource_group_name=rg_name,
+                account_name=storage_name,
+                parameters={
+                    "location": az_loc,
+                    "kind": "StorageV2",
+                    "sku": {"name": "Standard_LRS"}
+                }
+            )
+            account_result = poller.result()
+            print(f"Provisioned storage account {account_result.name}")
     else:
-        poller = storage_client.storage_accounts.begin_create(
-            resource_group_name=rg_name,
-            account_name=storage_name,
-            parameters={
-                "location": az_loc,
-                "kind": "StorageV2",
-                "sku": {"name": "Standard_LRS"}
-            }
-        )
-        account_result = poller.result()
-        print(f"Provisioned storage account {account_result.name}")
+        print(f"Storage account {storage_name} already exists in resource group: {rg_name}")
+
+except Exception as ex:
+    print(ex)
+
+try:
+    if not [item.name for item in storage_client.blob_containers.list(resource_group_name=rg_name, account_name=storage_name) if item.name=="container-1"]:
+
+        blob_client = storage_client.blob_containers.create(resource_group_name=rg_name,
+                                                        account_name=storage_name,
+                                                        container_name=container_name,
+                                                        blob_container={})
+    else:
+        print(f"Container {container_name} already exists!")
+
+
+except Exception as ex:
+    print(ex)
+
+try:
+    blob_service_client = BlobServiceClient(account_url="https://azpythonsdksa0806.blob.core.windows.net/", credential=creds)
+    container_client = blob_service_client.get_container_client(container=container_name)
+
+    for item in container_client.list_blob_names():
+        print(item)
+
+    # with open(file="test.txt", mode="rb") as data:
+    #     blob = container_client.upload_blob(name="test.txt", data=data, overwrite = True)
 
 except Exception as ex:
     print(ex)
